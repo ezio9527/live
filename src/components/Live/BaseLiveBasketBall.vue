7dba3e315aa33a2d29e85c65ef586297813c5d64
hzl <277341642@qq.com>
2021-01-09 06:24:54 +0800

    升级系统

--- src/components/Live/BaseLiveBasketBall.vue
+++ src/components/Live/BaseLiveBasketBall.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="live-basketball-text">
+    <p class="live-title">文字直播</p>
     <van-tabs type="card" v-model="tabActive">
       <van-tab title="第一节">
+        <van-steps direction="vertical" :active="-1">
+          <van-step>
+            <p><span class="title">第一节 00:00</span><span class="sub-title">18-9</span></p>
+            <p class="content">本节比赛开始</p>
+          </van-step>
+          <van-step>
+            <p><span class="title">第一节 00:15</span></p>
+            <p class="content">乔哈特 三分 跳投 不中</p>
+          </van-step>
+        </van-steps>
       </van-tab>
       <van-tab title="第二节"></van-tab>
       <van-tab title="第三节"></van-tab>
@@ -11,12 +22,14 @@
 </template>
 
 <script>
-import { Tab, Tabs } from 'vant'
+import { Tab, Tabs, Steps, Step } from 'vant'
 export default {
   name: 'BaseBasketBallText',
   components: {
     VanTab: Tab,
-    VanTabs: Tabs
+    VanTabs: Tabs,
+    VanSteps: Steps,
+    VanStep: Step
   },
   data () {
     return {
@@ -26,142 +39,53 @@ export default {
 }
 </script>
 
-<style scoped lang="less">
-.live-basketball {
+<style lang="less">
+.live-basketball-text{
   .px2vw(width, 700);
-  .px2vw(height, 540);
-  background: #FFFFFF;
-  box-shadow: 0px 5px 44px 0px rgba(0, 0, 0, 0.06);
   .px2vw(border-radius, 26);
+  background: #FFFFFF;
+  box-shadow: 0px 10px 88px 0px rgba(0, 0, 0, 0.06);
+  .px2vw(padding-left, 25);
+  .px2vw(padding-right, 25);
+  .px2vw(padding-top, 30);
+  .px2vw(padding-bottom, 30);
   -webkit-box-sizing: border-box;
   -moz-box-sizing: border-box;
   box-sizing: border-box;
-  .px2vw(padding-top, 15);
-  .px2vw(padding-bottom, 25);
-  .px2vw(padding-left, 25);
-  .px2vw(padding-right, 25);
-  .top {
-    display: -webkit-box;
-    display: -webkit-flex;
-    display: -ms-flexbox;
-    display: flex;
-    justify-content: space-between;
-    position: relative;
-    &:before {
-      width: 100%;
-      height: 1px;
-      background: #979797;
-      opacity: 0.3;
-      content: '';
-      position: absolute;
-      left: 0;
-      bottom: 0;
-    }
-    .left, .right {
-      display: -webkit-box;
-      display: -webkit-flex;
-      display: -ms-flexbox;
-      display: flex;
-      flex-direction: column-reverse;
-      .px2vw(margin-bottom, 24);
-    }
-    .left {
-      .px2vw(font-size, 24);
-      div {
-        .px2vw(width, 80);
-        .px2vw(height, 33);
-        .px2vw(line-height, 33);
-        text-align: right;
-        position: relative;
-        &:before {
-          .px2vw(width, 5);
-          .px2vw(height, 33);
-          content: '';
-          position: absolute;
-          left: 0;
-          top: 0;
-          border-radius: 10px;
-        }
-        &.home {
-          .px2vw(margin-bottom, 16);
-        }
-        &.home:before {
-          background: #F6BD35;
-        }
-        &.guest:before {
-          background: #27C5C3;
-        }
-      }
-    }
-    .right {
-      text-align: right;
-      .px2vw(font-size, 22);
-      .header, .body {
-        span {
-          display: inline-block;
-          .px2vw(width, 80);
-          .px2vw(height, 42);
-        }
-      }
-      .header {
-        color: #999999;
-      }
-      .body {
-        color: #333333;
-      }
-    }
+  .live-title {
+    text-align: center;
+    .px2vw(line-height, 42);
+    .px2vw(font-size, 30);
+    font-family: PingFangSC-Semibold, PingFang SC;
+    font-weight: 600;
+    color: #333333;
+    .px2vw(margin-bottom, 20);
   }
-  .bottom {
-    .px2vw(margin-top, 27);
-    display: -webkit-box;
-    display: -webkit-flex;
-    display: -ms-flexbox;
-    display: flex;
-    justify-content: space-between;
-    position: relative;
-    .left, .right {
-      display: -webkit-box;
-      display: -webkit-flex;
-      display: -ms-flexbox;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      align-items: center;
-      color: #333333;
-      .home {
-        color: #F6BD35;
-      }
-      .guest {
-        color: #27C5C3;
-      }
+  .van-step {
+    .title {
+      color: #999999;
+      .px2vw(font-size, 22);
+      -webkit-transform: scale(0.95);
+      -moz-transform: scale(0.95);
+      -ms-transform: scale(0.95);
+      -o-transform: scale(0.95);
+      transform: scale(0.95);
     }
-    .middle {
-      .item {
-        display: -webkit-box;
-        display: -webkit-flex;
-        display: -ms-flexbox;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        .progress {
-          display: inline-block;
-          .px2vw(width, 245);
-          .px2vw(margin-left, 28);
-          .px2vw(margin-right, 28);
-          span {
-            display: inline-block;
-            width: 100%;
-            font-size: 12px;
-            color: #333333;
-            text-align: center;
-            -webkit-transform: scale(0.95);
-            -moz-transform: scale(0.95);
-            -ms-transform: scale(0.95);
-            -o-transform: scale(0.95);
-            transform: scale(0.95);
-          }
-        }
-      }
+    .sub-title {
+      float: right;
+      color: #333333;
+      .px2vw(font-size, 24);
+      font-weight: 600;
+    }
+    .content {
+      .px2vw(width, 623);
+      .px2vw(line-height, 70);
+      .px2vw(border-radius, 10);
+      background: #FBFBFB;
+      color: #333333;
+      .px2vw(font-size, 24);
+      .px2vw(padding-left, 25);
+      .px2vw(padding-right, 25);
     }
   }
 }
